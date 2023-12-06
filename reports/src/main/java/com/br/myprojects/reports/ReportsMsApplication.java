90a4830f8023138da389c1c53f69288d128e998e
Julio Rocha <juliogabrielmr@gmail.com>
2023-12-06 15:20:10 -0300

    Add Reports MS

---
+++ reports/src/main/java/com/br/myprojects/reports/ReportsMsApplication.java
@@ -0,0 +1,13 @@
+package com.br.myprojects.reports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportsMsApplication.class, args);
+	}
+
+}
---
+++ reports/src/test/java/com/br/myprojects/reports/ReportsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.myprojects.reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
